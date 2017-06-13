23a275fcd5ba4537a693a9c1ac82968c6cb5d0a4
karkaim <kar_nin@yahoo.com.mx>
2017-06-13 14:22:50 -0500

    video en home y estilos

---
+++ page-inicio.php
@@ -0,0 +1,36 @@
+<?php
+
+  /**
+  *@desc A page. See single.php is for a blog post layout.
+  */
+
+  $ajaxload = false;
+
+if(isset($_GET['ajaxload']))
+    $ajaxload = $_GET['ajaxload'];
+
+if($ajaxload == false)
+    get_header();
+
+  
+  ?>
+
+	<article class="postWrapper" id="post-<?php the_ID(); ?>">
+            
+          <div class="postWrapper" id="post-home-video">
+              <video muted autoplay loop id="bgvid">
+                  <source src="<?php bloginfo('template_url'); ?>/video/VIAVERDE_WEB_HOME_FINAL.webm" type="video/webm">
+                  <source src="<?php bloginfo('template_url'); ?>/video/VIAVERDE_WEB_HOME_FINAL.m4v" type="video/mp4">
+                  <source src="<?php bloginfo('template_url'); ?>/video/VIAVERDE_WEB_HOME_FINAL.ogv" type="video/ogv">
+              </video>
+              <div id="homeArrow"></div>
+          </div>
+
+  </article>
+
+
+<?php
+
+
+  if($ajaxload == false)get_footer(); 
+?>
