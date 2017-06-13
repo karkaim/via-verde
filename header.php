c9f405f2766e19fc0e98cb7f22e07e0b09eb640f
trip <luisblst@gmail.com>
2017-06-13 10:50:55 -0500

    Fonts
    Fonts and css style

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
                 
                 @import url( <?php bloginfo('template_url'); ?>/scripts/lightbox/css/lightbox.css );
 	</style>
-
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/fonts/font-awesome.css" />
 	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
 	<link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="<?php bloginfo('atom_url'); ?>" />
