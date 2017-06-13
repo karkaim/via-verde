b10900865c47f61589e762fa7594dd8a3e0ac684
karkaim <kar_nin@yahoo.com.mx>
2017-06-13 13:11:18 -0500

    taxonomy files

---
+++ taxonomy-categorias_bloques.php
@@ -0,0 +1,50 @@
+<?php
+
+  /**
+  *@desc A page. See single.php is for a blog post layout.
+  */
+
+  $ajaxload = false;
+
+if(isset($_GET['ajaxload']))
+    $ajaxload = $_GET['ajaxload'];
+
+if($ajaxload == false)
+    get_header();
+
+  if (have_posts()) : while (have_posts()) : the_post();
+  ?>
+
+	<article class="postWrapper" id="post-<?php the_ID(); ?>">
+            
+            <div id="tumbnail">
+                <?php 
+                    if ( has_post_thumbnail() ) 
+                        the_post_thumbnail(); 
+                ?>
+            </div>
+                
+		<header>
+
+        <a href="<?php the_permalink(); ?>">
+		    <h1 class="postTitle"><?php the_title(); ?> TAXONOMY Cat Bloques</h1>
+        </a>
+
+		</header>
+      
+		<section class="post"><?php the_excerpt(); ?></section>
+		
+
+    </article>
+
+  <?php
+
+  endwhile; else: ?>
+
+    <p>Sorry, no pages matched your criteria.</p>
+
+<?php
+  endif;
+
+  if($ajaxload == false)get_footer(); 
+?>
